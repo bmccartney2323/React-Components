f676e85f44c702f22992a1e04009b97150ae909c
bmccartney2323 <65781099+bmccartney2323@users.noreply.github.com>
2020-10-20 15:14:04 -0400

    Finished lesson on creating components

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Heading from "./Heading.jsx"
+import LList from "./LList.jsx";
+
+function App() {
+return (
+  <div>
+    <Heading />
+    <LList />
+  </div>
+
+);
+}
+
+export default App;
